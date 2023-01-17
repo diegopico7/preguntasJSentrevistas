0abffa0156273a8226220543f1e1d9c849718c6b
diegopico7 <dgpicasso@hotmail.com>
2023-01-17 12:48:37 -0300

    subiendo las primeras 15 preguntas

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//reverse
+let texto = ["hola", "Chau"];
+
+const reversive = () => {
+  return texto.reverse("");
+};
+console.log(reversive());
+//funciones
+//anonima
+const anonimo = function () {
+  console.log("Funcion anónima");
+};
+//declarada
+function declarada() {
+  console.log("Funcion declarada");
+}
+//arrow
+const arrow = () => {
+  console.log("Funcion flecha");
+};
